8f6ffa1433c2b7eab5b41ab6434ce499a3d9decf
iWEX Infomatics <emails@iwex.in>
2025-12-17 01:04:23 +0530

    fix: remove build.json and use direct asset paths for CDN-based app

--- iwex_website/hooks.py
+++ iwex_website/hooks.py
@@ -15,10 +15,8 @@ app_license = "MIT"
 # app_include_js = "/assets/iwex_website/js/iwex_website.js"
 
 # include js, css files in header of web template
-web_include_css = "/assets/iwex_website/css/iwex_website.css"
-web_include_js = [
-	"/assets/iwex_website/js/iwex_website.js"
-]
+web_include_css = "/assets/iwex_website/css/styles.css"
+web_include_js = "/assets/iwex_website/js/main.js"
 
 # include custom scss in every website theme (without file extension ".scss")
 # website_theme_scss = "iwex_website/public/scss/website"
--- iwex_website/public/js/iwex_website.bundle.js
+++
@@ -1,3 +0,0 @@
-// Placeholder file for build system
-// The actual code is in main.js and will be bundled by Frappe's build system
-
